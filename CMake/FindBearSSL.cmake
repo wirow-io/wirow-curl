fb5643e76e966eeeb509da1087e4b176b1e3b7fa
Anton Adamansky <adamansky@gmail.com>
2021-01-08 18:22:51 +0700

    * cmake_policy(VERSION 3.16)

--- CMake/FindBearSSL.cmake
+++ CMake/FindBearSSL.cmake
@@ -24,7 +24,7 @@ find_path(BEARSSL_INCLUDE_DIRS bearssl.h)
 find_library(BEARSSL_LIBRARY bearssl)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(BEARSSL DEFAULT_MSG
+find_package_handle_standard_args(BearSSL DEFAULT_MSG
     BEARSSL_INCLUDE_DIRS BEARSSL_LIBRARY)
 
 mark_as_advanced(BEARSSL_INCLUDE_DIRS BEARSSL_LIBRARY)
